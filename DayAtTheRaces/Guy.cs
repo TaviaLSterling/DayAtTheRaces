7415efeb35ad499d8b4c7ffc9b9041b6d9721c09
Tavia Sterling <TaviaSterling@outlook.com>
2019-01-14 12:18:41 -0700

    PlaceBet Method

--- DayAtTheRaces/Guy.cs
+++ DayAtTheRaces/Guy.cs
@@ -14,18 +14,40 @@ namespace DayAtTheRaces
         public int Cash;
         public RadioButton MyRadioButton;
         public Label MyLabel;
+        
+        public Label MyLabel2;
 
         public void UpdateLabels()
         {
-            
+            MyRadioButton.Text = Name + " has $" + Cash;     
         }
-        public void ClearBet(int BetAmount, int DogToWin)
+        public void ClearBet()
         {
-
+            MyBet = null;
+            MyLabel2.Text = Name + " hasn't placed a bet";
+        }
+        public bool PlaceBet(int BetAmount, int DogToWin)
+        {
+            this.MyBet = new Bet() { Amount = BetAmount, Dog = DogToWin, Bettor = this};
+            if (BetAmount <= Cash)
+            {
+                MyLabel2.Text = this.Name + " bets " + BetAmount + " dollars on " + DogToWin;
+                this.UpdateLabels();
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(Name + " does not have enough to cover that bet ");
+                this.MyBet = null;
+                return false;
+            }
         }
         public void Collect(int Winner)
         {
-
+            if (MyBet != null)
+                Cash = Cash + MyBet.PayOut(Winner);
+            ClearBet();
+            UpdateLabels();
         }
     }
 }
