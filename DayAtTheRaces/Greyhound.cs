b222b25d767c50ce9c19ec35c0350dd2b1d8e258
Tavia Sterling <TaviaSterling@outlook.com>
2019-01-14 12:08:17 -0700

    Greyhound and Guy Classes

---
+++ DayAtTheRaces/Greyhound.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayAtTheRaces
+{
+    public class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public bool Winner = false;
+        public Random Randomizer;
+        public bool Run()
+        {
+            int move = Randomizer.Next(1, 6);
+            Location = Location + move;
+            MyPictureBox.Left = StartingPosition + Location;
+            if (MyPictureBox.Left >= RacetrackLength)
+            {
+                Winner = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void TakeStartingPosition()
+        {
+            Location = 0;
+            MyPictureBox.Left = StartingPosition;
+        }
+    }
+}
---
+++ DayAtTheRaces/Guy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayAtTheRaces
+{
+    public class Guy
+    {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
+
+        public void UpdateLabels()
+        {
+            
+        }
+        public void ClearBet(int BetAmount, int DogToWin)
+        {
+
+        }
+        public void Collect(int Winner)
+        {
+
+        }
+    }
+}
