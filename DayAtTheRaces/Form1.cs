14e2d8d4935304c9b090112b34a67433669af06d
Tavia Sterling <TaviaSterling@outlook.com>
2019-01-15 13:02:42 -0700

    pictures

--- DayAtTheRaces/Form1.cs
+++ DayAtTheRaces/Form1.cs
@@ -34,18 +34,20 @@ namespace DayAtTheRaces
         {
             InitializeComponent();
 
-            dogsArray[0] = new Greyhound() { MyPictureBox = racetrackPictureBox, Name = "Doggo", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = racetrackPictureBox.Left, Randomizer = Randomizer };
-            dogsArray[1] = new Greyhound() { MyPictureBox = pictureBox2, Name = "Jasper", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox2.Left, Randomizer = Randomizer };
-            dogsArray[2] = new Greyhound() { MyPictureBox = pictureBox3, Name = "Slim", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox3.Left, Randomizer = Randomizer };
-            dogsArray[3] = new Greyhound() { MyPictureBox = pictureBox4, Name = "Jim", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox4.Left, Randomizer = Randomizer };
+            dogsArray[0] = new Greyhound() { MyPictureBox = pictureBox1, Name = "Humphrey", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = racetrackPictureBox.Left, Randomizer = Randomizer };
+            dogsArray[1] = new Greyhound() { MyPictureBox = pictureBox2, Name = "Jasper", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox1.Left, Randomizer = Randomizer };
+            dogsArray[2] = new Greyhound() { MyPictureBox = pictureBox3, Name = "Slim", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox2.Left, Randomizer = Randomizer };
+            dogsArray[3] = new Greyhound() { MyPictureBox = pictureBox4, Name = "Jim", RacetrackLength = racetrackPictureBox.Width - racetrackPictureBox.Width, StartingPosition = pictureBox3.Left, Randomizer = Randomizer };
 
-            guyArray[0] = new Guy() { MyBet = null, Name = "Zack", Cash = 50, MyLabel = labelJoe, MyRadioButton = rbJoe, MyLabel2 = labelJoeBet };
-            guyArray[1] = new Guy() { MyBet = null, Name = "Nick", Cash = 75, MyLabel = labelBob, MyRadioButton = rbBob, MyLabel2 = labelBobBet };
-            guyArray[2] = new Guy() { MyBet = null, Name = "Taylor", Cash = 45, MyLabel = labelAl, MyRadioButton = rbAl, MyLabel2 = labelAlBet };
+            guyArray[0] = new Guy() { MyBet = null, Name = "Joe", Cash = 50, MyLabel = labelJoe, MyRadioButton = rbJoe, MyLabel2 = labelJoeBet };
+            guyArray[1] = new Guy() { MyBet = null, Name = "Bob", Cash = 75, MyLabel = labelBob, MyRadioButton = rbBob, MyLabel2 = labelBobBet };
+            guyArray[2] = new Guy() { MyBet = null, Name = "Al", Cash = 45, MyLabel = labelAl, MyRadioButton = rbAl, MyLabel2 = labelAlBet };
 
             guyArray[0].UpdateLabels();
             guyArray[1].UpdateLabels();
             guyArray[2].UpdateLabels();
+
+              
         }
     }
 }
